f0b1dc708fb68a331ffe537eb1506f6f741c6342
lexx <lexx@localhost>
2022-06-09 15:33:54 +0300

    fetcher update

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,24 +1,33 @@
 import type { IApiResponse } from 'pages/api/transactions';
 
-export function fetcher<T>(requestInfo: RequestInfo, ...args: any[]): Promise<T> {
-  return fetch(requestInfo, ...args).then((res) => res.json());
+export class ResponseError extends Error {
+  constructor(message: any) {
+    super(message);
+  }
+  public info: any;
+  public status: number | undefined;
 }
 
-export function fetcherApi<T>(requestInfo: RequestInfo, ...args: any[]): Promise<IApiResponse<T>> {
-  return fetch(requestInfo, ...args).then((res) => res.json());
+export function fetcher<T>(requestInfo: RequestInfo, ...args: any[]): Promise<T> {
+  return fetch(requestInfo, ...args).then(async (res) => {
+    if (!res.ok) {
+      const error = new ResponseError(res.statusText);
+      error.info = await res.json();
+      error.status = res.status;
+      throw error;
+    }
+    return res.json();
+  });
 }
 
 export function get<T>(
   path: string,
-  args: RequestInit = { method: "get" }
+  args: RequestInit = { method: 'get' }
 ): Promise<IApiResponse<T>> {
   return fetcher(new Request(path, args));
-};
+}
 
-export function post<T>(
-  path: string,
-  body: any,
-  ): Promise<IApiResponse<T>>  {
-    const args: RequestInit = { method: "post", body: JSON.stringify(body) }
-    return fetcher(new Request(path, args));
-};
+export function post<T>(path: string, body: any): Promise<IApiResponse<T>> {
+  const args: RequestInit = { method: 'post', body: JSON.stringify(body) };
+  return fetcher(new Request(path, args));
+}
