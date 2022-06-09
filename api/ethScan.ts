cee4f383b727f5e5c1373c001b225dd933d6fe05
lexx <lexx@localhost>
2022-06-09 15:34:25 +0300

    update etherScan

--- api/ethScan.ts
+++ api/ethScan.ts
@@ -6,50 +6,55 @@ type Params = { [key: string]: string | number | boolean };
 const apiBaseUrl = 'https://api.etherscan.io/api';
 const apiKey = 'NBI9SGSW6P1NZQGYT8BD8DDN5UQ7AIM4E9';
 
-const defaultParams: Params = {
-  module: 'proxy',
-  apikey: apiKey,
-};
+const defaultParams: Params = { apikey: apiKey };
 
 function sleep(ms: number) {
   return new Promise<void>((resolve) => setTimeout(resolve, ms));
 }
 
-const slowFetcher = (async (...args) => {
+interface IEtherScanApiResp {
+  result: object;
+}
+
+type IEtherScanApiResult = IEtherScanApiResp['result'];
+
+async function slowFetcher<T>(...args: Parameters<typeof fetcher>): Promise<T> {
   await sleep(300);
-  return fetcher(...args);
-}) as typeof fetcher;
-
-// class slowFetch {
-//   cnt: number;
-//   timeoutId: NodeJS.Timeout;
-//   constructor() {
-//     this.cnt = 0;
-//   }
-//   async dofetch(...params) {
-//     if(this.cnt++ >= 20) {
-//       await sleep(1000);
-//     }
-//   }
-// }
-
-export async function fetchRecentBlock(): Promise<any> {
-  const params = { ...defaultParams, action: 'eth_blockNumber' };
-  const url = getUrlWithParams(apiBaseUrl, params);
-  const resp = await slowFetcher(url);
-  return resp['result'];
+  return fetcher<T>(...args);
 }
 
-export async function fetchBlockByNumber(blockNumber: string): Promise<any> {
-  const params = { ...defaultParams, action: 'eth_getBlockByNumber' };
+export async function fetchRecentBlock(): Promise<IEtherScanApiResult> {
+  const params = {
+    ...defaultParams,
+    module: 'proxy',
+    action: 'eth_blockNumber',
+  };
   const url = getUrlWithParams(apiBaseUrl, params);
-  const resp = await slowFetcher(url);
-  return resp['result'];
+  const resp = await slowFetcher<IEtherScanApiResp>(url);
+  return resp.result;
 }
 
-export async function fetchTransactionByHash(hash: string): Promise<any> {
-  const params = { ...defaultParams, action: 'eth_getTransactionByHash' };
+export async function fetchBlockByNumber(blockNumber: string): Promise<IEtherScanApiResult> {
+  const params = {
+    ...defaultParams,
+    module: 'proxy',
+    action: 'eth_getBlockByNumber',
+    tag: blockNumber,
+    boolean: true,
+  };
   const url = getUrlWithParams(apiBaseUrl, params);
-  const resp = await slowFetcher(url);
-  return resp['result'];
+  const resp = await slowFetcher<IEtherScanApiResp>(url);
+  return resp.result;
+}
+
+export async function fetchTransactionByHash(hash: string): Promise<IEtherScanApiResult> {
+  const params = {
+    ...defaultParams,
+    module: 'proxy',
+    action: 'eth_getTransactionByHash',
+    txhash: hash,
+  };
+  const url = getUrlWithParams(apiBaseUrl, params);
+  const resp = await slowFetcher<IEtherScanApiResp>(url);
+  return resp.result;
 }
