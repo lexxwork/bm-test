c540d406e12eaf73ab5495a64c16d8e700fda74e
lexx <lexx@localhost>
2022-06-10 08:22:55 +0300

    add .container class for root element

--- views/TransactionsTableView/index.tsx
+++ views/TransactionsTableView/index.tsx
@@ -116,7 +116,7 @@ export const TransactionsTableView: React.FC = () => {
   }, [height]);
 
   return (
-    <div>
+    <div className={styles.container}>
       <div className={styles.containerSearch}>
         <SearchFilter options={filter} validateFn={validateSearch} onSearch={onSearch} />
       </div>
