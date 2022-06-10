5ce531b68a54e2d53e58e8545b772fa65ba87756
lexx <lexx@localhost>
2022-06-10 15:14:36 +0300

    Loader

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,10 @@
+import type { FC } from 'react';
+import styles from './index.module.scss';
+
+export const Loader: FC = () => {
+  return (
+//     <div className={styles.container}>
+      <div className={styles.loader}></div>
+//     </div>
+  );
+};
