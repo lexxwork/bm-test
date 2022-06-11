10e8dc7d91ec3e7b69022f77fe43d139307fbed9
lexx <lexx@localhost>
2022-06-11 22:10:29 +0300

    set option autoIndex in production check for not production first

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,11 +1,16 @@
 import { connect, Mongoose } from 'mongoose';
+import type { ConnectOptions } from 'mongoose';
 
 if (!process.env.MONGODB_URI) {
-  throw new Error('Please add your Mongo URI to .env.local');
+  throw new Error('Please add your Mongo URI to MONGODB_URI in .env.local');
 }
 
 const uri: string = process.env.MONGODB_URI;
-const options = {};
+const options: ConnectOptions = {};
+
+if (process.env.NODE_ENV === 'production') {
+  options.autoIndex = false; //TODO autoindex manually
+}
 
 type MongooseOrNull = Mongoose | null;
 let clientMongoose: MongooseOrNull;
@@ -16,7 +21,7 @@ declare global {
   var _mongooseClient: Mongoose | null;
 }
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV !== 'production') {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
   if (!global._mongoClientPromise) {
@@ -43,7 +48,7 @@ function getClientFn() {
 }
 
 export default async function initMongoose(): Promise<MongooseOrNull> {
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV !== 'production') {
     if (!global._mongooseClient) {
       global._mongooseClient = await getClientFn();
     }
